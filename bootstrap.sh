7fae6c1fc5a0ae5add1da0080b7991b724c10e7a
Tyler <tyler@cruftbusters.com>
2026-09-02 19:38:47 -0700

    add argocd install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,10 @@
+for executable in git kind kubectl argocd; do
+    if ! which $executable ; then
+        echo "$executable is not installed. Please install $executable and try again."
+        exit 1
+    fi
+done
+
 cd $HOME
 if [ ! -d .git ] ; then
     git init
@@ -11,3 +18,10 @@ git reset --hard origin/main
 if kind get clusters 2>&1 | grep "No kind clusters found"; then
     kind create cluster
 fi
+
+if ! kubectl get namespace argocd >/dev/null 2>&1; then
+    kubectl create namespace argocd
+fi
+
+# todo pin the version
+kubectl apply -n argocd --server-side --force-conflicts -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
