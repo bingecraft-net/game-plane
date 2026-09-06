46c4ce21552915f59d7a9e1e4b2db3818fa2a8e4
Tyler <tyler@cruftbusters.com>
2026-09-06 15:09:15 -0700

    remove github workflows and trim bootstrap script

--- bootstrap-scripts/bootstrap.sh
+++ bootstrap-scripts/bootstrap.sh
@@ -1,19 +1,9 @@
-for executable in git kind kubectl; do
+for executable in kind kubectl; do
     if ! which $executable ; then
         echo "$executable is not installed. Please install $executable and try again."
         exit 1
     fi
 done
-
-cd $HOME
-if [ ! -d .git ] ; then
-    git init
-    git remote add origin https://github.com/bingecraft-net/game-plane
-    git sparse-checkout init --cone
-fi
-git sparse-checkout set bootstrap-manifests
-git fetch origin main
-git reset --hard origin/main
 
 if kind get clusters 2>&1 | grep "No kind clusters found"; then
     kind create cluster
