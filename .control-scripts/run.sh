a2e563310ee2812f2f0c56c61c1f9b540066b48c
Tyler <tyler@cruftbusters.com>
2026-09-18 20:56:56 -0700

    control-scripts - mount dev, run, and local share containers in privileged

--- .control-scripts/run.sh
+++ .control-scripts/run.sh
@@ -5,4 +5,10 @@ trap 'echo "Error on line $LINENO"; exit 1' ERR
 
 podman build -t control-scripts -f .control-scripts/Containerfile .control-scripts
 
-podman run --rm control-scripts -- idpbuilder create --name "game-plane"
+podman run --rm \
+    --privileged \
+    --volume /dev:/dev \
+    --volume /run:/run \
+    --volume $HOME/.local/share/containers:/root/.local/share/containers \
+    control-scripts \
+    idpbuilder create --name "game-plane"
